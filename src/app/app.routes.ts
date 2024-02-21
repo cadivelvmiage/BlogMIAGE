8016edf0459b8d42c49e28f8a0fe2826faf5032e
Hassaan Is <is.hassaan@gmail.com>
2024-02-21 11:08:10 +0100

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { MessageComponent } from './message/message.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {'path':'message',"title":"message",component:MessageComponent}
+];
