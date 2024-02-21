11b5081dfcf23f6672c2039d31a1de735cd6f53d
Hassaan Is <is.hassaan@gmail.com>
2024-02-21 11:57:58 +0100

    navbar

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'message-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,HeaderComponent],
   templateUrl: './message.component.html',
   styleUrl: './message.component.css'
 })
